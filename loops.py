60b04f2dccfe7a4437f90fc1ef2dad9676340676
piyushverma11 <piyushvermaalm@gmail.com>
2022-03-11 11:02:44 +0530

    loops files

---
+++ loops.py
@@ -0,0 +1,55 @@
+# sum = 0 
+# num1 = int(input("enter a number1 "))
+# num2 = int(input("enter a number2 "))
+# for i in range (num1, num2):
+#     sum += i
+# print (sum)
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))  
+# for i in range (num1, num2 ):
+#    if (i % 2) == 0: 
+#       print(i)
+#    else:
+#       continue
+
+# num1 = int(input("enter number1: "))
+# num2 = int(input("enter number2: "))
+# for i in range (num1, num2):
+#    if i % 2 != 0:
+#       print(i)
+#    else:
+#       continue   
+# def divide(num1, num2):
+#    return num1, num2
+
+# num1 = int(input("enter number: "))
+# num2 = int(input("enter number: "))
+# division = divide(num1, num2)
+# for divide in range (num1, num2):
+#    if divide %2 == 0 and divide %4 == 0:
+#        print(divide)
+
+n = int(input ("enter the number in rows"))
+for i in range (1, n+1):
+   for j in range (1, i+1):
+      print (j, end= "")
+   print()
+      
+   
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
